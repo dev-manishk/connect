3f5ab01570228b91811c80e99f45a2f265d6edf1
Manish Kumar <70199850+dev-manishk@users.noreply.github.com>
2026-04-14 10:51:29 +0530

    Update ChatPage.jsx

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -96,7 +96,7 @@ const ChatPage = () => {
   if (loading || !chatClient || !channel) return <ChatLoader />;
 
   return (
-    <div className="h-[100vh]">
+    <div className="h-[94vh]">
       <Chat client={chatClient}>
         <Channel channel={channel}>
           <div className="w-full relative">
